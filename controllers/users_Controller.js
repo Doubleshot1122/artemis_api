82cbefadac16a140e177153de45a9bf6ad311453
Doubleshot1122 <skibo1122@yahoo.com>
2017-06-06 14:10:30 -0700

    removed debug logic and added error catcher

--- controllers/users_Controller.js
+++ controllers/users_Controller.js
@@ -8,23 +8,22 @@ function postNewUser(req, res, next) {
     password: req.body.password || 'required field',
     user_name: req.body.user_name || 'required field'
   }
-
-  console.log(req.body);
-  console.log(user);
+  
   if(user.display_name === 'required field'
     || user.email === 'required field'
     || user.icon === 'required field'
     || user.password === 'required field'
     || user.user_name === 'required field'){
-    console.log("======inside & error=====");
     user.error = "data is missing"
     res.json({user})
   } else {
-    console.log("======inside & success=====");
     db('users').insert(user, '*')
     .then(newUser => {
       console.log("success", newUser);
       res.json({newUser})
+    })
+    .catch(err => {
+      res.json(err)
     })
   }
 }
